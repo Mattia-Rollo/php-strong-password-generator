298cebd704a786371b22055bf2f997e76e326118
mattia.rollo <ste.tia@hotmail.it>
2022-11-29 17:33:45 +0100

    add new function passwordGen2

--- functions/functions.php
+++ functions/functions.php
@@ -1,33 +1,72 @@
 <?php
-function passwordGen($password, $options, $passLength, $repeat)
+// function passwordGen($password, $options, $passLength, $repeat)
+// {
+//     $lettere = 'abcdefghilmnopqrstuvz';
+//     $numeri = '1234567890';
+//     $simboli = '@%$!*#';
+
+
+
+//     if (isset($options) && isset($passLength)) {
+//         while (strlen($password) < intval($passLength)) {
+//             if (in_array('lettere', $options)) {
+//                 $numRand = rand(1, strlen($lettere));
+//                 $password .= substr($lettere, $numRand, 1);
+//             }
+//             if (in_array('lettereMaiuscole', $options)) {
+//                 $numRand = rand(1, strlen($lettere));
+//                 $password .= substr(strtoupper($lettere), $numRand, 1);
+//             }
+//             if (in_array('numeri', $options)) {
+//                 $numRand = rand(1, strlen($numeri));
+//                 $numFromString = substr($numeri, $numRand, 1);
+//                 if (str_contains($password, $numFromString) && ($repeat == 'false')) {
+//                 } else {
+//                     $password .= $numFromString;
+//                 }
+//             }
+//             if (in_array('simboli', $options)) {
+//                 $numRand = rand(1, strlen($simboli));
+//                 $password .= substr($simboli, $numRand, 1);
+//             }
+//         }
+//         return $password;
+//     }
+// }
+
+function passwordGen2($password, $options, $passLength, $repeat)
 {
-    $lettere = 'abcdefghilmnopqrstuvz';
-    $numeri = '1234567890';
+    $lettere = 'abcde'; //fghilmnopqrstuvz
+    $numeri = '123'; //4567890
     $simboli = '@%$!*#';
+    $stringAllInOne = '';
     if (isset($options) && isset($passLength)) {
+        if (in_array('lettere', $options)) {
+            $stringAllInOne .= $lettere;
+        }
+        if (in_array('lettereMaiuscole', $options)) {
+            $stringAllInOne .= strtoupper($lettere);
+        }
+        if (in_array('numeri', $options)) {
+            $stringAllInOne .= $numeri;
+        }
+        if (in_array('simboli', $options)) {
+            $stringAllInOne .= $simboli;
+        }
+
+
         while (strlen($password) < intval($passLength)) {
-            if (in_array('lettere', $options)) {
-                $numRand = rand(1, strlen($lettere));
-                $password .= substr($lettere, $numRand, 1);
-            }
-            if (in_array('lettereMaiuscole', $options)) {
-                $numRand = rand(1, strlen($lettere));
-                $password .= substr(strtoupper($lettere), $numRand, 1);
-            }
-            if (in_array('numeri', $options)) {
-                $numRand = rand(1, strlen($numeri));
-                $numFromString = substr($numeri, $numRand, 1);
-                if (str_contains($password, $numFromString) && ($repeat == 'false')) {
-                } else {
-                    $password .= $numFromString;
-                }
-            }
-            if (in_array('simboli', $options)) {
-                $numRand = rand(1, strlen($simboli));
-                $password .= substr($simboli, $numRand, 1);
+            $numRand = rand(1, strlen($stringAllInOne));
+            $character = substr($stringAllInOne, $numRand, 1);
+            if (str_contains($password, $character) && ($repeat == 'false')) {
+            } else {
+                $password .= substr($stringAllInOne, $numRand, 1);
+
             }
         }
-        return $password;
     }
+    return $password;
 }
+
+
 ?>
--- index.php
+++ index.php
@@ -3,109 +3,118 @@ session_start();
 include __DIR__ . '/partials/header.php';
 include __DIR__ . '/functions/functions.php';
 
-if (isset($_GET['options'])) {
-    var_dump($_GET['options']);
+if (isset($_POST['options'])) {
+    var_dump($_POST['options']);
 }
-if (isset($_GET['passLength'])) {
-    var_dump($_GET['passLength']);
+if (isset($_POST['passLength'])) {
+    var_dump($_POST['passLength']);
 }
-if (isset($_GET['repeat'])) {
-    var_dump($_GET['repeat']);
+if (isset($_POST['repeat'])) {
+    var_dump($_POST['repeat']);
 }
 // $lettere = 'abcdefghilmnopqrstuvz';
 // $numeri = '1234567890';
 // $simboli = '@%$!*#';
 
+$lettere = 'abcde'; //fghilmnopqrstuvz
+$numeri = '123'; //4567890
+$simboli = '@%$!*#';
+$stringAllInOne = '';
 
 $password = '';
-// if (isset($_GET['options']) && isset($_GET['passLength'])) {
-//     while (strlen($password) < intval($_GET['passLength'])) {
-//         if (in_array('lettere', $_GET['options'])) {
-//             $numRand = rand(1, strlen($lettere));
-//             $password .= substr($lettere, $numRand, 1);
-//         }
-//         if (in_array('lettereMaiuscole', $_GET['options'])) {
-//             $numRand = rand(1, strlen($lettere));
-//             $password .= substr(strtoupper($lettere), $numRand, 1);
-//         }
-//         if (in_array('numeri', $_GET['options'])) {
-//             $numRand = rand(1, strlen($numeri));
-//             $numFromString = substr($numeri, $numRand, 1);
-//             if (str_contains($password, $numFromString) && ($_GET['repeat'] == 'false')) {
-//             } else {
-//                 $password .= $numFromString;
-//             }
-//         }
-//         if (in_array('simboli', $_GET['options'])) {
-//             $numRand = rand(1, strlen($simboli));
-//             $password .= substr($simboli, $numRand, 1);
-//         }
-//     }
-//     echo $password;
+// if (isset($_POST['options'], $_POST['passLength'], $_POST['repeat'])) {
+//     echo passwordGen($password, $_POST['options'], $_POST['passLength'], $_POST['repeat']);
 // }
-echo passwordGen($password, $_POST['options'], $_POST['passLength'], $_POST['repeat']);
-
+if (isset($_POST['options'], $_POST['passLength'], $_POST['repeat'])) {
+    echo passwordGen2($password, $_POST['options'], $_POST['passLength'], $_POST['repeat']);
+}
 ?>
 
 <body>
-    <div class="container p-5 rounded">
-        <div class="row">
-            <div class="col">
-                <form action="./index.php" method="POST">
-                    <div class="mb-3 row">
+    <form action="./index.php" method="POST">
+        <div class="container p-5 rounded shadow-lg">
+            <div class="row">
+                <div class="col">
+                    <div class="pb-3 row">
                         <label for="password-length" class="col-sm-6 col-form-label">Lunghezza Password</label>
                         <div class="col-sm-6">
                             <input type="number" class="form-control" id="password-length" name="passLength" min="0"
                                 oninput="validity.valid||(value='');">
                         </div>
                     </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="lettere" name="options[]"
-                            id="Lettere-Maiuscole" checked>
-                        <label class="form-check-label" for="Lettere-Maiuscole">
-                            Lettere
-                        </label>
+                    <div class="repeat-radio py-3 row">
+                        <span class="col-sm-6">Consenti la ripetizione di caratteri?</span>
+                        <span class="col-sm-6">
+                            <input type="radio" class="btn-check" name="repeat" id="success-outlined" checked
+                                value="true">
+                            <label class="btn btn-outline-success" for="success-outlined">Si</label>
+                            <input type="radio" class="btn-check" name="repeat" value="false" id="danger-outlined">
+                            <label class="btn btn-outline-danger" for="danger-outlined">No</label>
+                        </span>
                     </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="Lettere-Maiuscole" value="lettereMaiuscole"
-                            name="options[]" checked>
-                        <label class="form-check-label" for="Lettere-Maiuscole">
-                            Lettere Maiuscole
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="options[]" value="numeri" id="Numeri"
-                            checked>
-                        <label class="form-check-label" for="Numeri">
-                            Numeri
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="options[]" value="simboli" id="Simboli"
-                            checked>
-                        <label class="form-check-label" for="Simboli">
-                            Simboli
-                        </label>
+                    <div class="checkbox py-3 row">
+                        <div class="col-sm-6">
+                            opzioni:
+                            <div>Lettere:
+                                <?php echo $lettere ?>
+                            </div>
+                            <div>Numeri:
+                                <?php echo $numeri ?>
+                            </div>
+                            <div>Simboli:
+                                <?php echo $simboli ?>
+                            </div>
+
+
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="lettere" name="options[]"
+                                    id="Lettere-Maiuscole" checked>
+                                <label class="form-check-label" for="Lettere-Maiuscole">
+                                    Lettere
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="Lettere-Maiuscole"
+                                    value="lettereMaiuscole" name="options[]" checked>
+                                <label class="form-check-label" for="Lettere-Maiuscole">
+                                    Lettere Maiuscole
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="options[]" value="numeri"
+                                    id="Numeri" checked>
+                                <label class="form-check-label" for="Numeri">
+                                    Numeri
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="options[]" value="simboli"
+                                    id="Simboli" checked>
+                                <label class="form-check-label" for="Simboli">
+                                    Simboli
+                                </label>
+                            </div>
+                        </div>
                     </div>
 
+                    <!-- input for reapet characters -->
+
+
+                    <!--###### bottoni ########-->
                     <div class="py-3 ">
                         <button type="submit" class="btn btn-primary">invia</button>
-                        <button type="reset" class="btn btn-primary">resetta</button>
+                        <button type="reset" class="btn btn-secondary">resetta</button>
                     </div>
 
+                    <!-- </div>
+                <div class="col"> -->
+                </div>
             </div>
-            <div class="col">
-                <select class="form-select" name="repeat">
-                    <option selected>Scegli</option>
-                    <option value="false">No repeat Number</option>
-                    <option value="true">Reapet Number</option>
 
-                </select>
-            </div>
-            </form>
         </div>
-
-    </div>
+    </form>
 </body>
 
 </html>

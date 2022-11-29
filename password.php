dd00125c50414292ac96b06d2bd8dd85aced0ca3
mattia.rollo <ste.tia@hotmail.it>
2022-11-29 18:45:47 +0100

    style

--- password.php
+++ password.php
@@ -1,12 +1,19 @@
 <?php
 session_start();
 include __DIR__ . '/partials/header.php';
-var_dump($_SESSION['password']);
+// var_dump($_SESSION['password']);
 ?>
 
 
 <body>
-
+    <div class="wrapper">
+        <div class="container" id="password">
+            <h1 class="text-center">La tua nuova password è</h1>
+            <h2 class="text-center text-danger ">
+                <?php echo $_SESSION['password'] ?>
+            </h2>
+        </div>
+    </div>
 </body>
 
 </html>
